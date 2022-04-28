4547dec31a925654d12ef0bc6d22d54d4a070870
maurice <Maurice.svip@gmail.com>
2022-04-28 22:51:20 +0800

    Delete src/webrtc_moudle directory

--- src/webrtc_moudle/rtc/video_track_receiver.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-// WebRTC
-#include <api/media_stream_interface.h>
-
-class VideoTrackReceiver {
-public:
-    virtual void AddTrack(webrtc::VideoTrackInterface* track) = 0;
-    virtual void RemoveTrack(webrtc::VideoTrackInterface* track) = 0;
-};
